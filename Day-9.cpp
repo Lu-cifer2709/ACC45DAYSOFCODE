968fa8c35d6425d7f1950ef3a1e433fe6316abcf
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-10-29 23:31:18 +0530

    Add files via upload

---
+++ Day-9.cpp
@@ -0,0 +1,21 @@
+//FEVER
+#include <iostream>
+using namespace std;
+
+int main() {
+ int n,t;
+ cin >> n;
+ for (int i = 0; i<n; i++){
+    cin >> t;
+
+ if(t>98)
+  {
+      cout << "Yes"<<endl;
+  }
+  else
+  {
+      cout << "No"<<endl;
+  }
+ }
+	return 0;
+}
