27355e5c63d3d49d70d4721608167764199d3176
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-10-31 20:30:50 +0530

    Add files via upload

---
+++ Day-11.cpp
@@ -0,0 +1,16 @@
+//MEAN MEDIAN
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        
+        cout<<y<<" "<<y<<" "<<(3*x) -(2*y)<<endl;
+    }
+    
+    return 0;
+}
