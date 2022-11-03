299615f97535a23d840bbe9f9025141ab11c8810
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-11-03 20:26:43 +0530

    Add files via upload

---
+++ Day-14.cpp
@@ -0,0 +1,17 @@
+//BEST OF TWO
+#include<iostream>
+using namespace std;
+
+int main(){
+int t,n,m;
+cin>>t;
+while(t--)
+{
+    cin>>n>>m;
+    if(n>=m)
+        cout<<n<<endl;
+        else
+        cout<<m<<endl;
+    }
+    return 0;
+}
