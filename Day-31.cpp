625c1556620c30f5cdf8482d24351fb2c695e07e
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-11-20 20:30:20 +0530

    Add files via upload

---
+++ Day-31.cpp
@@ -0,0 +1,13 @@
+//UWCOI21A
+#include <iostream>
+using namespace std;
+int main() {
+	int t ; 
+	cin >> t ; 
+	while(t--){
+	    int n ; 
+	    cin >> n ; 
+	    cout<<"1 "<< n <<endl ; 
+	}
+	return 0;
+}
