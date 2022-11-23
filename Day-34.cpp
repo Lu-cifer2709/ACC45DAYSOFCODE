5e72de9fe6d7bea3053a4873eab60ba9e6221fb1
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-11-23 20:31:08 +0530

    Add files via upload

---
+++ Day-34.cpp
@@ -0,0 +1,16 @@
+//CUTPAPER
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int N,K;
+	    cin>>N>>K;
+	    int S = N/K;
+	    cout<<S*S<<endl;
+	}
+	return 0;
+}
