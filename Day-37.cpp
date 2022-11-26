ec36139fe49bd772e329cf3da5e7e58ee81adf44
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-11-26 23:23:08 +0530

    Add files via upload

---
+++ Day-37.cpp
@@ -0,0 +1,16 @@
+// WATERCONSC
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x;
+        cin >> x;
+        if(x>=2000) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+	return 0;
+}
