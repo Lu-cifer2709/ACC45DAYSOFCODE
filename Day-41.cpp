fdf7c5e03fd79fa81f7bc4fb24100252769a9f37
Lu-cifer2709 <104576168+Lu-cifer2709@users.noreply.github.com>
2022-11-30 22:23:02 +0530

    Add files via upload

---
+++ Day-41.cpp
@@ -0,0 +1,16 @@
+//PRIMEDICE
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin >> t;
+while(t--)
+{
+    int a,b;
+    cin >> a >> b;
+    if(a+b == 2 || a+b == 3 || a+b == 5 || a+b == 7 || a+b == 11) cout << "Alice" << endl;
+    else cout << "Bob" << endl;
+}
+	return 0;
+}
